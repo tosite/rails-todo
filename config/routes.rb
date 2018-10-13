79ffd1562c63d3792f82823ef910af263cb4ac4c
erugis0345 <tl091264@gmail.com>
2018-10-13 09:57:04 +0900

    vue-files modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'sessions/new'
 
-  get       '/'       => 'users#index'
+  get       '/'       => 'sessions#new'
   get       'signup'  => 'users#new'
   get       '/login'  => 'sessions#new'
   post      '/login'  => 'sessions#create'
